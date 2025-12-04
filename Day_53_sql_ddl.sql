30ab0c6f3549508e1c2ea9cd6d805d97695dcab8
Muhammed Jaseem KP <muh.jaskp@gmail.com>
2025-12-04 23:57:27 +0530

    Create Day_53_sql_ddl.sql

---
+++ Day_53_sql_ddl.sql
@@ -0,0 +1,18 @@
+-- SQL DDL-Data Definition Language 
+-- Commands:
+
+-- CREATE 
+-- DROP 
+-- ALTER
+-- TRUNCATE 
+-- RENAME
+-- COMMENT 
+
+-- Example:
+
+CREATE TABLE employees (
+    employee_id INT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    hire_date DATE
+);
