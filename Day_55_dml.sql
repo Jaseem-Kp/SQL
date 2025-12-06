34f89a5e548100a484f41e502b10f311500d6b7c
Muhammed Jaseem KP <muh.jaskp@gmail.com>
2025-12-06 22:06:20 +0530

    Create Day_55_dml.sql

---
+++ Day_55_dml.sql
@@ -0,0 +1,11 @@
+-- SQL DML- Data Manipulation Language 
+-- Commands
+
+-- INSERT
+-- UPDATE 
+-- DELETE 
+
+-- Eg:- 
+
+INSERT INTO Employees(FirstName, LastName, Department)
+VALUES ('Jas','KP','HR');
